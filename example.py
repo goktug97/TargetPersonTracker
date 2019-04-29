843d4a6966e56666ea7c7d1b58a98ea8f1d564ff
Göktuğ Karakaşlı <20567087+goktug97@users.noreply.github.com>
2019-04-29 10:54:00 +0300

    change example

--- example.py
+++ example.py
@@ -10,16 +10,17 @@ class Example(tracker.Tracker):
         """Initiliaze tracker and image source."""
         tracker.Tracker.__init__(self, args)
 
-        self.cap = cv2.VideoCapture(self.args.input)
+        # This is the default
+        self._cap = cv2.VideoCapture(self.args.input)
         # Camera Settings
-        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.args.camera_width)
-        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.args.camera_height)
+        self._cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.args.camera_width)
+        self._cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.args.camera_height)
         if (self.args.camera_fps):
-            self.cap.set(cv2.CAP_PROP_FPS, self.args.camera_fps)
+            self._cap.set(cv2.CAP_PROP_FPS, self.args.camera_fps)
 
     def get_frame(self):
         """Overload get_frame function."""
-        ret, self.frame = self.cap.read()
+        ret, self.frame = self._cap.read()
         return ret, self.frame
 
 if __name__ == '__main__':
